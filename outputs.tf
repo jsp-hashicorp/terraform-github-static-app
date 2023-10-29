929d8e517f8b65bdf42ddd765ab83e717b62a4d8
jsp-hashicorp <jsp@hashicorp.com>
2023-10-30 07:55:33 +0900

    2nd commit

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,11 @@
 output "repo_url" {
   value       = github_repository.gh_repo.html_url
   description = "The URL of the created repository."
+  sensitive = true
 }
 
 output "app_url" {
   value       = "https://${var.destination_org}.github.io/${var.waypoint_project}"
   description = "The URL of the app on GitHub Pages."
+  sensitive = true
 }
